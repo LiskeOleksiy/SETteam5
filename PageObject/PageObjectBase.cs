f7833ee477c1327cee3214f0103b6b64d4cdee3a
Oleksiy Liske <liske2017@gmail.com>
2020-04-29 22:14:28 +0300

    PageObjectBase added

---
+++ PageObject/PageObjectBase.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+namespace Selenium1.PageObject
+{
+    public abstract class PageObjectBase
+    {
+        protected readonly IWebDriver Driver;
+        public PageObjectBase(IWebDriver driver) => Driver = driver;
+    }
+}
