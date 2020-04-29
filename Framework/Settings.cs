67da533ecb9993246b49ff03b3f92b3354003a14
Oleksiy Liske <liske2017@gmail.com>
2020-04-29 22:10:20 +0300

    Settings added

---
+++ Framework/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Selenium1.Framework
+{
+    public static class Settings
+    {
+        public static readonly string Url = "https://www.prestashop.com/en";
+        public static readonly TimeSpan ImplicitWait = TimeSpan.FromMilliseconds(Convert.ToInt32("3000"));
+    }
+
+}
