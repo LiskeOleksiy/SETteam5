eaa2a13799be421bb49a267f9e955b438b2cbea4
AlexejShpilovyj <62441096+AlexejShpilovyj@users.noreply.github.com>
2020-05-03 14:42:03 +0300

    Add test Shpylovyi

---
+++ Test/TestShpylovyi.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using AutomationTest.Framework;
+using AutomationTest.PageObject;
+using NUnit.Framework;
+
+namespace AutomationTest.Test
+{
+    [TestFixture]
+    public class Test2
+    {
+        private static readonly TimeSpan ImplicitWait = TimeSpan.FromMilliseconds(Convert.ToInt32("3000"));
+        private IWebDriver _driver;
+        private MainPage _mainPage;
+        private void DriverSettings()
+       
+        {
+           
+        }
+
+    }
+}
