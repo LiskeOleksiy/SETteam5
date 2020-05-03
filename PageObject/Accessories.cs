d2cef737cedf592c6f466a7633d239da910eddff
Oleksiy Liske <liske2017@gmail.com>
2020-05-03 18:27:56 +0300

    bin deleted

--- PageObject/Accessories.cs
+++ PageObject/Accessories.cs
@@ -6,18 +6,21 @@ namespace AutomationTest.PageObject
     {
         private static readonly By ChooseAccessoriesButton =
             By.XPath("/ html / body / main / header / div[2] / div / div[1] / div[2] / div[1] / ul / li[2] / a");
+
         public Accessories(IWebDriver driver) : base(driver)
         {
         }
+
         public Accessories ChooseAccessories()
         {
             InputData(ChooseAccessoriesButton);
             return this;
 
-            public Accessories ClickSort()
+             Accessories ClickSort()
             {
-                InputData(SortButton);
+
                 return this;
             }
+        }
     }
 }
