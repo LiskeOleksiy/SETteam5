7f2b879de1d0c225d25b9a59a9e19a12556bac0f
AlexStrokov <62788259+AlexStrokov@users.noreply.github.com>
2020-05-03 16:19:26 +0300

    Add files via upload

---
+++ PageObject/Accessories.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomationTest.PageObject
+{
+    public class Accessories : PageObjectBase
+    {
+        private static readonly By ChooseAccessoriesButton =
+            By.XPath("/ html / body / main / header / div[2] / div / div[1] / div[2] / div[1] / ul / li[2] / a");
+        public Accessories(IWebDriver driver) : base(driver)
+        {
+        }
+        public Accessories ChooseAccessories()
+        {
+            InputData(ChooseAccessoriesButton);
+            return this;
+
+            public Accessories ClickSort()
+            {
+                InputData(SortButton);
+                return this;
+            }
+    }
+}
